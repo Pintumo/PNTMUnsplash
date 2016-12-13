bd1d734427783357ce4795d574a2af3b12e00474
Evangelos Sismanidis <evangelos@posteo.eu>
2016-12-13 12:48:50 +0100

    rx method for photos endpoint

--- PNTMUnsplash/PNTMUnsplash.swift
+++ PNTMUnsplash/PNTMUnsplash.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+import Alamofire
+import RxSwift
+
+
+
+struct PNTMUnsplash {
+    
+    static let baseUrl = "https://api.unsplash.com"
+    static let clientId = "PLEASE ADD YOUR UNSPLASH API KEY HERE"
+    
+    static func photos() -> Observable<[String:Any]> {
+        
+        return Observable<[String:Any]>.create() { observer in
+            let request = Alamofire.request("\(baseUrl)/photos?client_id=\(clientId)")
+                .validate()
+                .responseJSON { response in
+                        switch response.result {
+                        case .success:
+                            if let jsonArray = response.result.value as? [[String:Any]] {
+                                for json in jsonArray {
+                                    observer.on(.next(json))
+                                }
+                                observer.on(.completed)
+                            } else {
+                                observer.on(.error(RxError.unknown))
+                            }
+                            
+                        case .failure(let error):
+                            observer.on(.error(error))
+                        }
+            }
+            
+            return Disposables.create() {
+                request.cancel()
+            }
+        }.map { json in
+            guard let id = json["id"] as! String?,
+                let urls = json["urls"] as! [String:Any]?,
+                let photo = urls["regular"] as! String? else {
+                throw RxError.unknown
+            }
+            
+            return [ "id" : id, "photo" : photo]
+        }
+    }
+}
+
+
